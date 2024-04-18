6cc19687a6b652c54a3bfccbf9c97d8fdc7a36cb
Bona Philippe <bona.lukengu@mukuru.com>
2024-04-18 21:32:29 +0200

    Payment Log Update

--- src/Payment.php
+++ src/Payment.php
@@ -17,5 +17,11 @@ class Payment extends ApiClient
 		return $this->execute('POST', $data);
 	}
 
+	public function updatePaymentLog(int $id, array $data): array
+	{
+		$this->path = 'payments/log/'.$id;
+		return $this->execute('PUT', $data);
+	}
+
 
 }
