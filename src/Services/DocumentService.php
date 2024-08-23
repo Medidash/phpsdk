78559dca45d4a0c3861e79f7b3ccad8e4c4c19b7
Bona Philippe <bona.lukengu@mukuru.com>
2024-08-23 05:29:17 +0200

    Phatmacy documents

---
+++ src/Services/DocumentService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Medidash\Phpsdk\Services;
+
+class DocumentService extends BaseService
+{
+
+	public function pharmacyDocuments(int $pharmacyId): array
+	{
+		return $this->get("/documents/{$pharmacyId}");
+	}
+
+}
