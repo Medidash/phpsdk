9ac49030e4bb722f428202a6fbe06f54f656e957
Bona Philippe <bona.lukengu@mukuru.com>
2024-08-28 22:28:36 +0200

    add create

--- src/Services/PharmacyService.php
+++ src/Services/PharmacyService.php
@@ -44,19 +44,30 @@ class PharmacyService extends BaseService
 	{
 		return $this->get('pharmacy/provinces');
 	}
-
+	/**
+	 * @param int $pharmacyId
+	 * @return array
+	 */
 	public function activate(int $pharmacyId): array
 	{
 		$data = ['active' => '1'];
 		return $this->put('pharmacy/'.$pharmacyId, $data);
 	}
 
+	/**
+	 * @param int $pharmacyId
+	 * @return array
+	 */
 	public function deactivate(int $pharmacyId): array
 	{
 		$data = ['active' => '0'];
 		return $this->put('pharmacy/'.$pharmacyId, $data);
 	}
 
+	/**
+	 * @param array $data
+	 * @return array
+	 */
 	public function create(array $data): array
 	{
 		return $this->post('pharmacy', $data);
